d2328f455e500228bce6fc430e9b59ec0ca62d49
rivva <rivvarivvs@gmail.com>
2020-12-26 14:46:44 +0000

    initialized snake body into the field

--- Snake.c
+++ Snake.c
@@ -2,10 +2,30 @@
 
 #define N 20
 #define M 40
-int i, j;
+int i, j, Field[N][M],x,y,Gy,Head,Tail;
 
-void main()
-{
+void snakeInitialization () {
+    for (i=0;i<N;i++) {
+        for(j=0;j<M;j++) {
+            Field[i][j] = 0;
+        }
+    }
+
+    x=N/2;
+    y=M/2;
+    Head=5;
+    Tail=1;
+    Gy=y;
+
+    for(i=0; i<Head; i++) {
+        Gy++;
+        Field[x][Gy-Head] = i+1;
+    }
+
+
+}
+
+void print() {
     for(i=0; i<=M+1;i++) {
         if (i==0) {
             printf("%c", 201);
@@ -20,14 +40,14 @@ void main()
 
     for(i=0; i<N; i++) {
         printf("%c", 186);
-        for(j=0; j<=M; j++) {
-            if(j == M) {
-                printf("%c\n", 186);
-            } else {
-                printf(" ");
+
+        for(j=0; j<M; j++) {
+            if(Field[i][j] == 0) printf(" ");
+            if(Field[i][j] > 0 && Field[i][j]!= Head) printf("%c", 176);
+            if(Field[i][j] == Head) printf("%c", 178);
+            if(j==M-1) printf("%c\n", 186);
             }
         }
-    }
 
 
     for(i=0; i<=M+1;i++) {
@@ -39,4 +59,10 @@ void main()
             printf("%c", 205);
         }
     }
+}
+
+void main()
+{
+    snakeInitialization();
+    print();
 }
